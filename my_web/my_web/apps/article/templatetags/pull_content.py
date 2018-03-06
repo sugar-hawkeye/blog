b614a27522e88f3053d02c75c8902c29135a2276
poster797 <wim.poster797@gmail.com>
2018-03-06 11:42:02 +0800

    delete uunnecessary

--- my_web/my_web/apps/article/templatetags/pull_content.py
+++ my_web/my_web/apps/article/templatetags/pull_content.py
@@ -1,16 +1,16 @@
-from django import template
-
-from django.utils.safestring import mark_safe
-from django.utils.encoding import force_text
-from django.template.defaultfilters import stringfilter
-
-from my_web.libs.Utils import read_md
-
-
-register = template.Library()
-
-@register.filter(is_safe=True)
-@stringfilter
-def pull_content(value):
-    content = read_md(value)
-    return mark_safe(force_text(content,'utf-8'))
+# from django import template
+#
+# from django.utils.safestring import mark_safe
+# from django.utils.encoding import force_text
+# from django.template.defaultfilters import stringfilter
+#
+# from my_web.libs.Utils import read_md
+#
+#
+# register = template.Library()
+#
+# @register.filter(is_safe=True)
+# @stringfilter
+# def pull_content(value):
+#     content = read_md(value)
+#     return mark_safe(force_text(content,'utf-8'))
