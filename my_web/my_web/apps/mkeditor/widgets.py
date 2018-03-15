0d4182441f576f68c6e0ec3cf1ca53dbe1292249
poster797 <wim.poster797@gmail.com>
2018-03-15 13:55:00 +0800

    修改mkeditor settings

--- my_web/my_web/apps/mkeditor/widgets.py
+++ my_web/my_web/apps/mkeditor/widgets.py
@@ -1,35 +1,30 @@
 from django import VERSION, forms
-from django.contrib.admin import widgets as admin_widgets
 from django.template import Context, loader
-from django.utils.safestring import mark_safe
 from django.utils.html import conditional_escape
-
+from mkeditor import settings
 
 try:
     from django.forms.utils import flatatt
 except ImportError:
-    from django.forms.util import flatatt # <1.7
+    from django.forms.util import flatatt
 
 try:
     from django.utils.encoding import force_unicode
-except ImportError:  # python3
+except ImportError:
     from django.utils.encoding import force_text as force_unicode
 
 class MarkdownWidget(forms.Textarea):
 
-
     def __init__(self, *args, **kwargs):
-        self.template = "editor.html"
-        # self.show_preview = kwargs.pop("show_preview", mkeditor_settings.SHOW_PREVIEW)
-        # self.template_name = kwargs.pop("template", mkeditor_settings.WIDGET_TEMPLATE)
-        # self.css = kwargs.pop("css", mkeditor_settings.WIDGET_CSS)
+        self.template = kwargs.pop("template", settings.WIDGET_TEMPLATE)
+        self.css = kwargs.pop("css", settings.WIDGET_CSS)
         super(MarkdownWidget, self).__init__(*args, **kwargs)
 
     @property
     def media(self):
         return forms.Media(
             css={
-                "all": ("css/editormd.css","css/style.css")
+                "all": self.css
             },
             js=(
                 "js/jquery.min.js",
@@ -44,11 +39,7 @@ class MarkdownWidget(forms.Textarea):
             final_attrs = self.build_attrs(attrs, name=name)
         else:
             final_attrs = self.build_attrs(attrs, {'name': name})
-
-
         template = loader.get_template(self.template)
-
-
         context = {
             "attrs": flatatt(final_attrs),
             "body": conditional_escape(force_unicode(value)),
